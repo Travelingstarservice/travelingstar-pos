546b1aff26872d207a363c2490eacbc7b79ae095
almightyallah96 <almightyallah96@gmail.com>
2025-12-24 12:52:32 -0500

    Fix require to use npm router package instead of missing local file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-// Old (broken)
 const router = require('./router');
-
-// Fixed (using npm package)
-const Router = require('router');     // note uppercase by convention
+const Router = require('router');   // uppercase by convention
 const router = Router();
+app.use(router);
 
